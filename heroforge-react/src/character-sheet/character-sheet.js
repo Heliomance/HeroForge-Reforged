5a350fe84c9a0a198a120bc5166847aacd5bac59
Richard Kille <rjkille@gmail.com>
2019-04-21 17:52:45 +0100

    Begun Character sheet work

---
+++ heroforge-react/src/character-sheet/character-sheet.js
@@ -0,0 +1,169 @@
+import React, {Component} from "react";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import CharacterAbilityScores from "./character-abilities";
+import {withStyles} from "@material-ui/core";
+
+class CharacterSheet extends Component {
+
+  render() {
+    const {character} = this.props;
+
+    return (
+      <Grid container direction={"column"}>
+        <Grid item container direction={"row"} spacing={8}> {/*Descriptive details*/}
+          <Grid item xs={4}>
+            <TextField label={"Character Name"} fullWidth/>
+          </Grid>
+          <Grid item xs={4}>
+            <TextField label={"Player"} fullWidth/>
+          </Grid>
+        </Grid>
+
+        <Grid item container direction={"row"} spacing={8}>
+          <Grid item xs={4}>
+            <TextField label={"Classes/Levels"} fullWidth/>
+          </Grid>
+          <Grid item xs={2}>
+            <TextField label={"Gender"} fullWidth/>
+          </Grid>
+          <Grid item xs={1}>
+            <TextField label={"Alignment"} fullWidth/>
+          </Grid>
+          <Grid item xs={1}>
+            <TextField label={"Deity"} fullWidth/>
+          </Grid>
+        </Grid>
+
+        <Grid item container direction={"row"} spacing={8}>
+          <Grid item xs={1}>
+            <TextField label={"Race"} fullWidth/>
+          </Grid>
+          <Grid item xs={1}>
+            <TextField label={"Size"} fullWidth/>
+          </Grid>
+          <Grid item xs={1}>
+            <TextField label={"Age"} fullWidth/>
+          </Grid>
+          <Grid item xs={1}>
+            <TextField label={"Height"} fullWidth/>
+          </Grid>
+          <Grid item xs={1}>
+            <TextField label={"Weight"} fullWidth/>
+          </Grid>
+          <Grid item xs={1}>
+            <TextField label={"Eyes"} fullWidth/>
+          </Grid>
+          <Grid item xs={1}>
+            <TextField label={"Hair"} fullWidth/>
+          </Grid>
+          <Grid item xs={1}>
+            <TextField label={"Skin"} fullWidth/>
+          </Grid>
+        </Grid>
+
+        <Grid item container direction={"row"} spacing={8}> {/*begin stat block*/}
+          <Grid item xs={4}>
+            <CharacterAbilityScores abilities={character.abilities}/>
+          </Grid>
+          <Grid item xs={8} container direction={"row"}>
+            <Grid item xs={2}>HP</Grid>
+            <Grid item xs={4}>wnds</Grid>
+            <Grid item xs={2}>nonlethal</Grid>
+            <Grid item xs={4}>Spd</Grid>
+
+            <Grid item xs={6} container direction={"row"}>
+              <Grid item xs={2}>AC</Grid>
+              <Grid item xs={2}>Touch</Grid>
+              <Grid item xs={2}>FF</Grid>
+              <Grid item xs={4}>INIT</Grid>
+              <Grid item xs={2}>SR</Grid>
+
+              <Grid item xs={12}>DR</Grid>
+            </Grid>
+
+            <Grid item xs={6}>Skills Header</Grid>
+          </Grid>
+        </Grid>
+
+        <Grid item container direction={"row"}>{/*main block*/}
+          <Grid item xs={8} container direction={"row"}>{/*stuff*/}
+            <Grid item xs={8} container direction={"column"}>
+              <Grid item xs container direction={"row"}>
+                <Grid item xs={4}>Saving Throws</Grid>
+                <Grid item xs>Total</Grid>
+                <Grid item xs>Base Save</Grid>
+                <Grid item xs>Ability Modifier</Grid>
+                <Grid item xs>Misc Modifier</Grid>
+                <Grid item xs>Temp Modifier</Grid>
+              </Grid>
+              <Grid item xs container direction={"row"}>
+                <Grid item xs={4}>Fortitude</Grid>
+                <Grid item xs>
+                  <TextField fullWidth disabled variant={"outlined"} type={"number"}/>
+                </Grid>
+                <Grid item xs>
+                  <TextField fullWidth disabled variant={"outlined"} type={"number"}/>
+                </Grid>
+                <Grid item xs>
+                  <TextField fullWidth disabled variant={"outlined"} type={"number"}/>
+                </Grid>
+                <Grid item xs>
+                  <TextField fullWidth disabled variant={"outlined"} type={"number"}/>
+                </Grid>
+                <Grid item xs>
+                  <TextField fullWidth variant={"outlined"} type={"number"}/>
+                </Grid>
+              </Grid>
+
+              <Grid item xs container direction={"row"}>
+                <Grid item xs={4}>Reflex</Grid>
+                <Grid item xs>
+                  <TextField fullWidth disabled variant={"outlined"} type={"number"}/>
+                </Grid>
+                <Grid item xs>
+                  <TextField fullWidth disabled variant={"outlined"} type={"number"}/>
+                </Grid>
+                <Grid item xs>
+                  <TextField fullWidth disabled variant={"outlined"} type={"number"}/>
+                </Grid>
+                <Grid item xs>
+                  <TextField fullWidth disabled variant={"outlined"} type={"number"}/>
+                </Grid>
+                <Grid item xs>
+                  <TextField fullWidth variant={"outlined"} type={"number"}/>
+                </Grid>
+              </Grid>
+
+              <Grid item xs container direction={"row"}>
+                <Grid item xs={4}>Will</Grid>
+                <Grid item xs>
+                  <TextField fullWidth disabled variant={"outlined"} type={"number"}/>
+                </Grid>
+                <Grid item xs>
+                  <TextField fullWidth disabled variant={"outlined"} type={"number"}/>
+                </Grid>
+                <Grid item xs>
+                  <TextField fullWidth disabled variant={"outlined"} type={"number"}/>
+                </Grid>
+                <Grid item xs>
+                  <TextField fullWidth disabled variant={"outlined"} type={"number"}/>
+                </Grid>
+                <Grid item xs>
+                  <TextField fullWidth variant={"outlined"} type={"number"}/>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item xs={4}>Conditionals</Grid>
+          </Grid>
+
+          <Grid item xs={4} container direction={"row"}>{/*skills*/}
+            Skills
+          </Grid>
+        </Grid>
+      </Grid>
+    );
+  }
+}
+
+export default CharacterSheet;
---
+++ heroforge-react/src/theme.js
@@ -0,0 +1,9 @@
+import {createMuiTheme} from "@material-ui/core";
+
+const myTheme = createMuiTheme({
+  typography: {
+    useNextVariants: true,
+  }
+});
+
+export default myTheme;
